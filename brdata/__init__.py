1fcc55ba58e5c67f165b3f8b6647410c99a4bd78
Gabriel Guarisa <gabrielguarisa@gmail.com>
2022-03-27 14:45:18 -0300

    Versão 0.5.0

--- brdata/__init__.py
+++ brdata/__init__.py
@@ -1,20 +1,4 @@
 """brdata"""
-import sys
+from . import b3, cvm, fundamentus, xpi
 
-if sys.version_info >= (3, 8):
-    from importlib import metadata as importlib_metadata
-else:
-    import importlib_metadata  # pragma: no cover
-
-from . import cvm, fundamentus, xpi
-
-
-def get_version() -> str:
-    try:
-        return importlib_metadata.version(__name__)
-    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
-        return "unknown"
-
-
-__version__: str = get_version()
-__all__: list = ["__version__", "xpi", "fundamentus", "cvm"]
+__all__: list = ["b3", "cvm", "fundamentus", "xpi"]
