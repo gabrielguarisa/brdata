df1ffe98e692df20167d27924cc0fdcc643c44d9
Gabriel Guarisa <gabrielguarisa@gmail.com>
2022-08-11 18:57:02 -0300

    Mudando de fii_proventos para proventos

--- brdata/statusinvest.py
+++ brdata/statusinvest.py
@@ -3,16 +3,16 @@ import pandas as pd
 from brdata.utils import get_response
 
 
-def fii_proventos(
+def proventos(
     papel: str,
     group_by_year: bool = False,
     group_by_year_col: str = "Última Data Com",
     provents_type: int = 2,
 ) -> pd.DataFrame:
-    """Histórico de proventos de um fundo imobiliário.
+    """Histórico de proventos de um fundo imobiliário ou ação.
 
     Args:
-        papel (str): Nome do fundo imobiliário.
+        papel (str): Símbolo do ativo.
         group_by_year (bool, optional): Agrupa os proventos por ano. Defaults to False.
         group_by_year_col (str, optional): Coluna usada para agrupamento por ano. Defaults to "Última Data Com".
         provents_type (int, optional): Tipo de provento. Defaults to 2.
@@ -40,6 +40,4 @@ def fii_proventos(
     return df[["Última Data Com", "Data de Pagamento", "Valor"]]
 
 
-# TODO: Proventos das ações.
-
-__all__ = ["fii_proventos"]
+__all__ = ["proventos"]
