33b40313e8d7135fe2daa022ab07ca9612d014b1
guarisadavi <guarisadavi@gmail.com>
2026-07-31 15:24:27 -0300

    refactor: change date format to fetch selic

--- src/brdata/bacen/selic.py
+++ src/brdata/bacen/selic.py
@@ -1,37 +1,49 @@
 import requests
-from datetime import date
+from datetime import date, datetime
 from typing import Literal
 
-from .utils import write_to_disk
+from .utils import write_to_disk, date_validator
 
 def fetch_selic(
-        category: Literal["meta", "diaria"],
-        start_date: str = None,
-        end_date: str = None,
-        path: str = None
+    category: Literal["meta", "diaria"],
+    start_date: str = None,
+    end_date: str = None,
+    path: str = None
 ):
-    
     """
     Fetches 'Selic Meta' or 'Selic Diária' data.
     It can be downloaded directly if a file path is provided.
-    \nDate format: str = 'dd/mm/yyyy'
-    \n- The difference between the start and end dates cannot exceed 10 years.
+    
+    Date format: str = 'YYYY-MM-DD'
+    - The difference between the start and end dates cannot exceed 10 years.
     """
-    start_date = start_date or date.today().strftime('%d/%m/%Y')
+
+    if start_date:
+        date_validator(start_date) 
+        parsed_start = datetime.strptime(start_date, "%Y-%m-%d")
+        valid_start_date = parsed_start.strftime("%d/%m/%Y")
+        file_date_suffix = start_date
+    else:
+        today = date.today()
+        valid_start_date = today.strftime("%d/%m/%Y")
+        file_date_suffix = today.strftime("%Y-%m-%d")
 
     if category == "meta":
         url_bcb = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.432/dados"
-        filename = f"selic_meta_{start_date.replace("/", "-")}.json"
+        filename = f"selic_meta_{file_date_suffix}.json"
     else:
         url_bcb = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados"
-        filename = f"selic_diaria_{start_date.replace("/", "-")}.json"
+        filename = f"selic_diaria_{file_date_suffix}.json"
 
     params = {
         "formato": "json",
-        "dataInicial": start_date
+        "dataInicial": valid_start_date
     }
+
     if end_date:
-        params["dataFinal"] = end_date
+        date_validator(end_date)
+        parsed_end = datetime.strptime(end_date, "%Y-%m-%d")
+        params["dataFinal"] = parsed_end.strftime("%d/%m/%Y")
 
     try:
         response = requests.get(url_bcb, params=params)
